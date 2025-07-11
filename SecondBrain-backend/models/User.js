8cc070bb8dd83c53b5c6c9ef1bcf3b4e3625a2bb
satyam0777 <officialsatyam0777@gmail.com>
2025-07-11 14:56:24 +0530

    schema user

---
+++ SecondBrain-backend/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+
+
+// User Schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  avatar: {
+    type: String,
+    default: null
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+export default mongoose.model('User', userSchema);
